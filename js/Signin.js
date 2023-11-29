8d4acf804a1999cffd78b97f6a32fd9fc9f2cd1c
Slime5 <akarius12@gmail.com>
2023-11-29 12:20:38 -0500

    testing google login

--- js/Signin.js
+++ js/Signin.js
@@ -1,47 +1,66 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDZIAVnBvB-GHlaDDO2GbOFjQhVvleb344",
-  authDomain: "database2023test.firebaseapp.com",
-  projectId: "database2023test",
-  storageBucket: "database2023test.appspot.com",
-  messagingSenderId: "352598568614",
-  appId: "1:352598568614:web:b1acd2e65514f8050f8e67",
-  measurementId: "G-6EF9ZJX352"
-}; 
+    apiKey: "AIzaSyBLKAChIxXjfnJv2pF0j7odVf96qVVkqL8",
+    authDomain: "cscia225.firebaseapp.com",
+    projectId: "cscia225",
+    storageBucket: "cscia225.appspot.com",
+    messagingSenderId: "122711494384",
+    appId: "1:122711494384:web:0a695ba2da3a5b669bbc35"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // save the data
-$('#Login').submit(function (e) {
-  e.preventDefault();
-  // get the user name and password from form
-  // You need to change this.
-  var email = 'yilianz4@gmail.com';
-  var password = 'ddsgagafda';
+$("#submit").click(function (e) {
+    e.preventDefault();
+    // get the user name and password from form
+    // You need to change this.
+    var email = "akarius12@gmail.com";
+    var password = "abc123";
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((success) => {
-      // Signed in
-      // ...
-      console.log('login in');
-      let user = firebase.auth().currentUser;
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(success => {
+            // Signed in
+            // ...
+            console.log("login in");
+            let user = firebase.auth().currentUser;
 
-      //user.updateProfile({ displayName: "Not sure" });
-      if (user != null) {
-        name = user.displayName;
-        email = user.email;
-        photoUrl = user.photoURL;
-        emailVerified = user.emailVerified;
-        console.log(name, email, emailVerified);
-      }
-    })
-    .catch((error) => {
-
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage);
-    });
+            //user.updateProfile({ displayName: "Not sure" });
+            if (user != null) {
+                name = user.displayName;
+                email = user.email;
+                photoUrl = user.photoURL;
+                emailVerified = user.emailVerified;
+                console.log(name, email, emailVerified);
+            }
+        })
+        .catch(error => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage);
+        });
 });
 
-// add  a google login choice here 
+// add  a google login choice here
+$("#google").click(e => {
+    // e.preventDefault();
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
+            var user = result.user;
+            console.log("sign in through google", user);
+        })
+        .catch(error => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+});
