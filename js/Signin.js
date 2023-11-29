175054927aaa0c15b675d4413ef70083ef8e4b7c
Slime5 <akarius12@gmail.com>
2023-11-29 12:35:56 -0500

    trying with redirect

--- js/Signin.js
+++ js/Signin.js
@@ -48,19 +48,12 @@ $("#google").click(e => {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase
         .auth()
-        .signInWithPopup(provider)
+        .signInWithRedirect(provider)
         .then(result => {
             var user = result.user;
             console.log("sign in through google", user);
         })
         .catch(error => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+            console.log(error);
         });
 });
